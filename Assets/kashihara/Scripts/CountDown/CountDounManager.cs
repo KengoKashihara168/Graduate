2fd4d33dd7c64561b305f688efcc59e988153054
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-10-28 10:00:47 +0900

    Revert "プレイヤーの生死情報を削除"
    This reverts commit 39ecbd39d42b4139357127e99b839825725d9fc5.

--- Assets/kashihara/Scripts/CountDown/CountDounManager.cs
+++ Assets/kashihara/Scripts/CountDown/CountDounManager.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 
 public class CountDounManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] playerImages;
     [SerializeField] private Text turnText;
 
     // Start is called before the first frame update
     void Start()
     {
+        for(int i = 0;i < (int)PlayerNum.MaxPlayer;i++)
+        {
+            if(!PlayerController.Instance.IsLive(i))
+            {
+                playerImages[i].GetComponent<ImageChange>().Change(1);
+            }
+        }
         PlayerController.Instance.IncrimentTurn();
         turnText.text = PlayerController.Instance.GetCurrentTurn().ToString();
     }
