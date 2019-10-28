a40a9a098c89cd66507af38e824c4d0407da6d0c
TSITCL\s172050 <KengoKashihara1998@gmail.com>
2019-10-28 16:10:17 +0900

    CSVファイルの読み込みテスト

---
+++ Assets/kashihara/Scripts/CSVTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVTest : MonoBehaviour
+{
+    [SerializeField] private TextAsset csvFile; // CSVファイル
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(csvFile.text.ToString());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
